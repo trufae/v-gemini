105e7f579628ff6b70d8c4981d16f8bf82d26d65
pancake <pancake@nopcode.org>
2023-09-05 13:50:39 +0000

    Refactor the server logic to make the code a bit more reusable

--- test-server.v
+++ test-server.v
@@ -1,52 +1,73 @@
 import gemini
 import os
 
+fn respond(mut query gemini.Query) ! {
+	mut response := ''
+	mut code := gemini.StatusCode.not_found
+	// append to any /? move outside here
+	if query.path.starts_with('/') {
+		eprintln('massage path')
+		query.path = '.${query.path}'
+	}
+	if query.path == '' || query.path.ends_with('/') {
+		query.path += 'index.gmi'
+	}
+	mut mime := 'text/gemini'
+	if query.path == '' {
+		response = 'hello world\n'
+		code = .success
+	} else {
+		if query.path == 'search' {
+			if query.data != '' {
+				response = '# Ok\n' + 'We search for ${query.data}\n' + '=> search again\n' +
+					'=> / root\n'
+				code = .success
+			} else {
+				code = .input
+				// mime = 'text/gemini'
+				mime = ''
+				response = '' // 'What is your question?'
+			}
+		} else if query.path.ends_with('.gmi') {
+			eprintln('=> ${query.path}')
+			data := os.read_file(query.path) or {
+				query.respond(int(gemini.StatusCode.not_found), 'file not found', '')!
+				return
+			}
+			code = .success
+			response = data
+		}
+	}
+	query.respond(int(code), mime, response)!
+}
+
+fn valid(mut query gemini.Query) bool {
+	query.path.index('..') or { return true }
+	return false
+	/*
+	dotdot := query.path.index('..') or { -1 }
+	if query.path.starts_with('/') || dotdot != -1 {
+		return false
+	}
+	return true
+	*/
+}
+
 fn main() {
 	mut s := gemini.serve(gemini.ServerOptions{})!
-	mut arg := 'text/gemini'
 	for {
-		arg = 'text/gemini'
+		// mut mime := 'text/gemini'
 		mut query := s.accept() or {
 			eprintln(err)
 			continue
 		}
-		dotdot := query.path.index('..') or { -1 }
-		if query.path.starts_with('/') || dotdot != -1 {
-			query.respond(51, 'invalid path', '')!
-			continue
-		}
-		if query.path == '' {
-			query.path = 'index.gmi'
-		}
 		dump(query)
-		mut response := ''
-		mut code := gemini.StatusCode.not_found
-		arg = 'text/gemini'
-		if query.path == '' {
-			response = 'hello world\n'
-			code = .success
-		} else {
-			if query.path == 'search' {
-				if query.data != '' {
-					response = '# Ok\n' + 'We search for ${query.data}\n' + '=> search again\n' +
-						'=> / root\n'
-					code = .success
-				} else {
-					code = .input
-					// arg = 'text/gemini'
-					arg = ''
-					response = '' // 'What is your question?'
-				}
-			} else if query.path.ends_with('.gmi') {
-				eprintln('=> ${query.path}')
-				data := os.read_file(query.path) or {
-					query.respond(51, 'file not found', '')!
-					continue
-				}
-				code = .success
-				response = data
+		if valid(mut query) {
+			respond(mut query) or {
+				eprintln(err)
 			}
+		} else {
+			query.respond(51, 'invalid path', '')!
 		}
-		query.respond(int(code), arg, response)!
 	}
 }
